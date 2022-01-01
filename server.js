5b2a4aabdbabca6757198d6a909f032b4c99fd17
jitasek <jitka.moreira@gmail.com>
2022-01-01 17:25:30 +0100

    prepare and set up files for routes, helpers etc)

--- server.js
+++ server.js
@@ -1,8 +1,24 @@
 const express = require("express");
 const connection = require("./config/connection");
+const routes = require("./controllers");
+const path = require("path");
+const helpers = require("./utils/helpers");
+const exphbs = require("express-handlebars");
+const hbs = exphbs.create({ helpers });
+const session = require("express-session");
+const { urlencoded } = require("express");
+const SequelizeStore = require("connect-session-sequelize")(session.Store); // to store session in mySQL
 
 const app = express();
 const PORT = process.env.PORT || 3306;
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(routes);
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
