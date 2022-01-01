4d9ac3cbf5931bac2564a3a513fbd7a9c3875fe6
jitasek <jitka.moreira@gmail.com>
2022-01-01 23:00:23 +0100

    seed Comment

--- seeds/index.js
+++ seeds/index.js
@@ -2,6 +2,7 @@ const seedUsers = require("./user-seeds");
 //const User = require("../models/User");
 const sequelize = require("../config/connection");
 const seedPosts = require("./post-seeds");
+const seedComments = require("./comment-seeds");
 
 // const createUsers = async (seedUsers) => {
 //   return await User.bulkCreate(seedUsers);
@@ -9,9 +10,10 @@ const seedPosts = require("./post-seeds");
 
 const seed = async () => {
   await sequelize.sync({ force: true });
-  // Create users
+  // Create users, posts, comments
   await seedUsers();
   await seedPosts();
+  await seedComments();
 
   await sequelize.close();
 };
