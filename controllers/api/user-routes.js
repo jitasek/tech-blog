a728f1f40b9a9ddb79dd1fbdf6e3e5501cecbe69
jitasek <jitka.moreira@gmail.com>
2022-01-03 15:37:07 +0100

    add route Get all users

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -2,9 +2,19 @@ const router = require("express").Router();
 const { User, Post, Comment } = require("../../models");
 
 // Get all users
-// router.get("/", (req, res) => {
-//   User.findAll({});
-// });
+router.get("/", async (req, res) => {
+  try {
+    const allUsers = await User.findAll({
+      attributes: {
+        exclude: ["password"],
+      },
+    });
+    res.status(200).json(allUsers);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json("Error retrieving all users data from database.");
+  }
+});
 
 // Get one user
 
