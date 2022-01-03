52c93977f666ec139df3176f5605de770ee245a8
jitasek <jitka.moreira@gmail.com>
2022-01-03 22:36:43 +0100

    add route Get one post

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -37,6 +37,41 @@ router.get("/", async (req, res) => {
 });
 
 // Get one post
+router.get("/:id", async (req, res) => {
+  try {
+    const allPosts = await Post.findByPk(req.params.id, {
+      attributes: ["id", "title", "content", "created_at"],
+      include: [
+        {
+          model: User,
+          attributes: ["username"],
+        },
+        {
+          model: Comment,
+          attributes: [
+            "id",
+            "comment_text",
+            "post_id",
+            "user_id",
+            "created_at",
+          ],
+          include: {
+            model: User,
+            attributes: ["username"],
+          },
+        },
+      ],
+    });
+    if (!allPosts) {
+      res.status(404).json("No post with this ID in the database");
+      return;
+    }
+    res.status(200).json(allPosts);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json("Error retrieving post data from database.");
+  }
+});
 
 // Create post
 
