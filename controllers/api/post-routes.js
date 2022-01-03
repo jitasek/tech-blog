305a2d9375fe7fe162f32ed8dc4d4f16ecbe60c5
jitasek <jitka.moreira@gmail.com>
2022-01-03 22:24:18 +0100

    add route Get all posts

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,7 +1,40 @@
 const router = require("express").Router();
+
 const { User, Post, Comment } = require("../../models");
 
 // Get all posts
+router.get("/", async (req, res) => {
+  try {
+    const allPosts = await Post.findAll({
+      attributes: ["id", "title", "content", "created_at"],
+      order: [["created_at", "DESC"]],
+      include: [
+        {
+          model: User,
+          attributes: ["username"],
+        },
+        {
+          model: Comment,
+          attributes: [
+            "id",
+            "comment_text",
+            "post_id",
+            "user_id",
+            "created_at",
+          ],
+          include: {
+            model: User,
+            attributes: ["username"],
+          },
+        },
+      ],
+    });
+    res.status(200).json(allPosts);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json("Error retrieving all posts data from database.");
+  }
+});
 
 // Get one post
 
