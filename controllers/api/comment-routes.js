89750285faeebb35ed72179be861c597c9b8bb8f
jitasek <jitka.moreira@gmail.com>
2022-01-04 13:59:20 +0100

    add route Get one comment

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -27,6 +27,35 @@ router.get("/", async (req, res) => {
 });
 
 // Get one comment
+router.get("/:id", async (req, res) => {
+  try {
+    const allComments = await Comment.findByPk(req.params.id, {
+      attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+      include: [
+        {
+          model: User,
+          attributes: ["username"],
+        },
+        {
+          model: Post,
+          attributes: ["id", "title", "content", "created_at"],
+          include: {
+            model: User,
+            attributes: ["username"],
+          },
+        },
+      ],
+    });
+    if (!allComments) {
+      res.status(404).json("No comment with this ID in the database");
+      return;
+    }
+    res.status(200).json(allComments);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json("Error retrieving comment data from database.");
+  }
+});
 
 // Create comment
 
