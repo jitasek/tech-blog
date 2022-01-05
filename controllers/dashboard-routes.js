e4f6f8f40762a551413be88e6f55a8b564b6fc00
jitasek <jitka.moreira@gmail.com>
2022-01-05 19:44:12 +0100

    add dashboard route Get all posts

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -3,4 +3,69 @@ const sequelize = require("../config/connection");
 
 const { User, Post, Comment } = require("../models");
 const { post } = require("./home-routes");
-// const withAuth = require("../utils/auth");
+const withAuth = require("../utils/auth");
+
+// find all users posts
+router.get("/", async (req, res) => {
+  try {
+    const allPosts = await Post.findAll({
+      // where: {
+      //   user_id: req.session.user_id,
+      // },
+      attributes: ["id", "title", "content", "created_at"],
+      include: [
+        {
+          model: Comment,
+          attributes: [
+            "id",
+            "comment_text",
+            "post_id",
+            "user_id",
+            "created_at",
+          ],
+          include: {
+            model: User,
+            attributes: ["username"],
+          },
+        },
+        {
+          model: User,
+          attributes: ["username"],
+        },
+      ],
+    });
+    // response
+    const posts = allPosts.map((post) => post.get({ plain: true }));
+    // res.render("dashboard", { posts, loggedIn: true });
+    res.render("dashboard");
+  } catch (error) {
+    console.log(error);
+    res.status(500).json("Error retrieving posts data from database.");
+  }
+});
+
+// get one of user's post
+router.get("/post/:id", async (req, res) => {
+  try {
+    const allPosts = await Post.findOne({
+      where: {
+        id: req.params.id,
+      },
+      attributes: ["id", "title", "content", "created_at"],
+    });
+  } catch (error) {}
+});
+
+// Edit (update) post
+
+// Delete post
+
+// Get all comments
+
+// Get one comment
+
+// Edit (update) comment
+
+// Delete comment
+
+module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -3,9 +3,11 @@ const router = require("express").Router();
 const apiRoutes = require("./api");
 const authRoutes = require("./auth");
 const homeRoutes = require("./home-routes.js");
+const dashboardRoutes = require("./dashboard-routes.js");
 
 router.use("/api", apiRoutes);
 router.use("/auth", authRoutes);
+router.use("/dashboard", dashboardRoutes);
 router.use("/", homeRoutes);
 
 module.exports = router;
