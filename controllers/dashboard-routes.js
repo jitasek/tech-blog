ae07746b9d495c7b19f0ea5965d71379f99e11d2
jitasek <jitka.moreira@gmail.com>
2022-01-06 12:45:53 +0100

    add handlebars for post and dashboard

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -44,7 +44,7 @@ router.get("/", async (req, res) => {
   }
 });
 
-// get one of user's post
+// get one of user's posts
 router.get("/post/:id", async (req, res) => {
   try {
     const allPosts = await Post.findOne({
@@ -52,17 +52,70 @@ router.get("/post/:id", async (req, res) => {
         id: req.params.id,
       },
       attributes: ["id", "title", "content", "created_at"],
+      include: [
+        {
+          model: Comment,
+          attributes: [
+            "id",
+            "comment_text",
+            "post_id",
+            "user_id",
+            "created_at",
+          ],
+          include: {
+            model: User,
+            attributes: ["username"],
+          },
+        },
+        {
+          model: User,
+          attributes: ["username"],
+        },
+      ],
     });
-  } catch (error) {}
+    // response
+    res.render("dashboard");
+  } catch (error) {
+    console.log(error);
+    res.status(500).json("Error retrieving post data from database.");
+  }
 });
 
 // Edit (update) post
+router.put("/post/edit/:id", async (req, res) => {
+  try {
+    const data = await Post.findByPk(req.params.id);
+    const post = data.get({plain: true})
+    // response
+    res.render("dashboard", post);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json("Error retrieving post data from database.");
+  }
+});
 
 // Delete post
+router.delete("/post/:id", async (req, res) => {
+  try {
+    await Post.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+  } catch (error) {
+    console.error(error.message);
+    return ??? ;
+  }
+});
+
+// Add (create) post
 
 // Get all comments
 
 // Get one comment
+
+// Add(create) comment
 
 // Edit (update) comment
 
